75e3694f99eff0dbd89d8521c3ad4fb2ede85ace
BlacklistModz <BlacklistModz.Ton@gmail.com>
2017-12-22 17:33:54 +0700

    123456

--- controllers/packing.php
+++ controllers/packing.php
@@ -1,1 +1,23 @@
-packing.php
+<?php
+class Packing extends Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index($id=null){
+
+    }
+    public function add(){
+
+    }
+    public function edit($id=null){
+
+    }
+    public function save(){
+    	if( empty($_POST) ) $this->error();
+    }
+    public function del($id=null){
+    	
+    }
+}
